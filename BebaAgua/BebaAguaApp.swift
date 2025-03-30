34669f7977655ae5a7765e8af3a638c6e16a0f9e
Kleyson Tavares <kleyson.tavares@icloud.com>
2025-03-30 17:10:20 -0300

    Initial Commit

---
+++ BebaAgua/BebaAguaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BebaAguaApp.swift
+//  BebaAgua
+//
+//  Created by Kleyson Tavares on 30/03/25.
+//
+
+import SwiftUI
+
+@main
+struct BebaAguaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BebaAgua/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BebaAgua
+//
+//  Created by Kleyson Tavares on 30/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
