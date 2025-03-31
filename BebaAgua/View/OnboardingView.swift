7a22076a4fb58248969c0d42177156c66a934e7f
Kleyson Tavares <kleyson.tavares@icloud.com>
2025-03-31 00:08:27 -0300

    Personaliza tela de cadastro inicial do usuario e adiciona preview

--- BebaAgua/View/OnboardingView.swift
+++ BebaAgua/View/OnboardingView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct OnboardingView: View {
     @State private var selectedGender = "Masculino"
-    @State private var birthYear = 2000
+    @State private var age = 18
     @State private var weight = 70.0
     @State private var dailyGoal = 2000.0
     @State private var reminderInterval = 60.0
@@ -21,65 +21,105 @@ struct OnboardingView: View {
         formatter.numberStyle = .none
         return formatter
     }()
-    
+
     var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("Sexo")) {
-                    Picker("Sexo", selection: $selectedGender) {
-                        Text("Masculino").tag("Masculino")
-                        Text("Feminino").tag("Feminino")
-                    }
-                    .pickerStyle(SegmentedPickerStyle())
-                }
-                
-                Section(header: Text("Ano de nascimento")) {
-                    HStack {
-                        Picker("Ano de Nascimento", selection: $birthYear) {
-                            ForEach(1900...2025, id: \ .self) { year in
-                                Text("\(numberFormatter.string(from: NSNumber(value: year)) ?? "\(year)")").tag(year)
+            NavigationView {
+                ZStack {
+                    Image("background")
+                        .resizable()
+                        .ignoresSafeArea()
+                    VStack {
+                        ScrollView {
+                            Text("Perfil").font(.title2).foregroundColor(.white)
+                            VStack(spacing: 20) {
+                                Section(header: Text("Sexo").font(.headline).foregroundColor(.white)) {
+                                    Picker("Sexo", selection: $selectedGender) {
+                                        Text("Masculino").tag("Masculino")
+                                        Text("Feminino").tag("Feminino")
+                                    }
+                                    .pickerStyle(SegmentedPickerStyle())
+                                    .padding()
+                                    .background(RoundedRectangle(cornerRadius: 20).fill(Color.blue))
+                                }
+                                Divider().colorInvert()
+
+                                HStack(spacing: 80) {
+                                    VStack {
+                                        Text("Idade")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                        
+                                        Picker("Idade", selection: $age) {
+                                            ForEach(1...100, id: \.self) { year in
+                                                Text("\(numberFormatter.string(from: NSNumber(value: year)) ?? "\(year)")").tag(year)
+                                            }
+                                            .foregroundColor(.white)
+                                        }
+                                        .pickerStyle(WheelPickerStyle())
+                                        .frame(width: 120, height: 120)
+                                    }
+                                    
+                                    VStack {
+                                        Text("Peso (kg)")
+                                            .font(.headline)
+                                            .foregroundColor(.white)
+                                        
+                                        Picker("Peso", selection: $weight) {
+                                            ForEach(1...200, id: \.self) { value in
+                                                Text("\(Int(value))").tag(Double(value))
+                                            }
+                                            .foregroundColor(.white)
+                                        }
+                                        .pickerStyle(WheelPickerStyle())
+                                        .frame(width: 120, height: 120)
+                                    }
+                                }
+                                Divider().colorInvert()
+                                
+                                Section(header: Text("Meta diária").font(.headline).foregroundColor(.white)) {
+                                    Slider(value: $dailyGoal, in: 500...5000, step: 100)
+                                    Text("\(Int(dailyGoal)) ml").foregroundColor(.white)
+                                }
+                                Divider().colorInvert()
+                                
+                                Section(header: Text("Intervalo das notificações").font(.headline).foregroundColor(.white)) {
+                                    Slider(value: $reminderInterval, in: 15...180, step: 15)
+                                    Text("\(Int(reminderInterval)) min").foregroundColor(.white)
+                                }
+                                Divider().colorInvert()
                             }
+                            .padding()
                         }
-                        .pickerStyle(WheelPickerStyle())
-                        .frame(height: 100)
-                    }
-                }
-                
-                Section(header: Text("Peso em kg")) {
-                    HStack {
-                        Picker("Peso", selection: $weight) {
-                            ForEach(1...200, id: \ .self) { value in
-                                Text("\(Int(value))").tag(Double(value))
-                            }
+
+                        Spacer()
+                        
+                        Button("Salvar e Continuar") {
+                            isOnboardingComplete = true
+                            UserDefaults.standard.set(selectedGender, forKey: "gender")
+                            UserDefaults.standard.set(age, forKey: "age")
+                            UserDefaults.standard.set(weight, forKey: "weight")
+                            UserDefaults.standard.set(dailyGoal, forKey: "dailyGoal")
+                            UserDefaults.standard.set(reminderInterval, forKey: "reminderInterval")
                         }
-                        .pickerStyle(WheelPickerStyle())
-                        .frame(height: 100)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .cornerRadius(20)
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 20)
+                        .padding(.bottom, 20)
                     }
+                    .ignoresSafeArea(.keyboard, edges: .bottom)
                 }
-                
-                Section(header: Text("Meta diária")) {
-                    Slider(value: $dailyGoal, in: 1000...5000, step: 100)
-                    Text("\(Int(dailyGoal)) ml")
+                .fullScreenCover(isPresented: $isOnboardingComplete) {
+                    ContentView()
                 }
-                
-                Section(header: Text("Intervalo das notificações")) {
-                    Slider(value: $reminderInterval, in: 15...180, step: 15)
-                    Text("\(Int(reminderInterval)) min")
-                }
-                
-                Button("Salvar e Continuar") {
-                    isOnboardingComplete = true
-                    UserDefaults.standard.set(selectedGender, forKey: "gender")
-                    UserDefaults.standard.set(birthYear, forKey: "birthYear")
-                    UserDefaults.standard.set(weight, forKey: "weight")
-                    UserDefaults.standard.set(dailyGoal, forKey: "dailyGoal")
-                    UserDefaults.standard.set(reminderInterval, forKey: "reminderInterval")
-                }
-            }
-            .navigationTitle("Configuração Inicial")
-            .fullScreenCover(isPresented: $isOnboardingComplete) {
-                ContentView()
             }
         }
     }
-}
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            OnboardingView()
+        }
+    }
