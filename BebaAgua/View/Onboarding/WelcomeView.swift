8187d034c18e851d89212c892ade62ffe009bade
Kleyson Tavares <kleyson.tavares@icloud.com>
2025-04-01 11:55:40 -0300

    Corrige  o botão para chamar a onboarding

--- BebaAgua/View/Onboarding/WelcomeView.swift
+++ BebaAgua/View/Onboarding/WelcomeView.swift
@@ -9,33 +9,35 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            
-            VStack(alignment: .leading, spacing: 20) {
-                Text("Oi")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
+        NavigationView {
+            VStack {
+                Spacer()
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Oi")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
                     
-                Text("Para fornecer conselhos de hidratação personalizados, preciso obter algumas informações básicas.")
-                    .font(.title2)
-                    .fontWeight(.bold)
+                    Text("Para fornecer conselhos de hidratação personalizados, preciso obter algumas informações básicas.")
+                        .font(.title2)
+                        .fontWeight(.bold)
+                }
+                .padding(.horizontal, 24)
+                
+                Spacer()
+                
+                NavigationLink(destination: OnboardingView()) {
+                    Text("Vamos começar")
+                        .fontWeight(.bold)
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(20)
+                        .padding(.horizontal, 24)
+                }
+                .padding(.bottom, 40)
             }
-            .padding(.horizontal, 24)
-            
-            Spacer()
-            
-            NavigationLink(destination: OnboardingView()) {
-                Text("Vamos começar")
-                    .fontWeight(.bold)
-                    .frame(maxWidth: .infinity)
-                    .padding()
-                    .background(Color.blue)
-                    .foregroundColor(.white)
-                    .cornerRadius(20)
-                    .padding(.horizontal, 24)
-            }
-            .padding(.bottom, 40)
         }
     }
 }
