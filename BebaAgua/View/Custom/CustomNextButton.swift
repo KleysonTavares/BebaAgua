99faea246f674330fffff0b24559aab96c1f7d93
Kleyson Tavares <kleyson.tavares@icloud.com>
2025-04-03 20:51:23 -0300

    Padroniza botão próximo do onboarding

--- BebaAgua/View/Custom/CustomNextButton.swift
+++ BebaAgua/View/Custom/CustomNextButton.swift
@@ -5,4 +5,25 @@
 //  Created by Kleyson Tavares on 02/04/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomNextButton: ViewModifier {
+    @Environment(\.presentationMode) var presentationMode
+
+    func body(content: Content) -> some View {
+        content
+            .padding()
+            .frame(width: 100)
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .fontWeight(.bold)
+            .clipShape(RoundedRectangle(cornerRadius: 20))
+    }
+}
+
+extension View {
+    func customNextButton() -> some View {
+        self.modifier(CustomNextButton())
+    }
+}
+
