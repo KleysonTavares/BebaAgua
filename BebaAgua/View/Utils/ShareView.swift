3cfc30b91ad50c931ec1d436cf2175ee436c79ee
Kleyson Tavares <kleyson.tavares@icloud.com>
2025-05-01 17:39:36 -0300

    cria ShareView

--- BebaAgua/View/Utils/ShareView.swift
+++ BebaAgua/View/Utils/ShareView.swift
@@ -5,4 +5,15 @@
 //  Created by Kleyson Tavares on 01/05/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct ActivityView: UIViewControllerRepresentable {
+    var activityItems: [Any]
+    var applicationActivities: [UIActivity]? = nil
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        return UIActivityViewController(activityItems: activityItems, applicationActivities: applicationActivities)
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {}
+}
